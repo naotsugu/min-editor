8fcf1eb67b311212d3a54a668ccf2eb4faa5b3c4
Naotsugu Kobayashi <naotsugukobayashi@gmail.com>
2023-06-27 19:25:10 +0900

    Modify TextLine

--- modules/model-text/src/main/java/com/mammb/code/editor2/model/layout/TextLine.java
+++ modules/model-text/src/main/java/com/mammb/code/editor2/model/layout/TextLine.java
@@ -18,7 +18,6 @@ package com.mammb.code.editor2.model.layout;
 import com.mammb.code.editor2.model.layout.impl.TextLineImpl;
 import com.mammb.code.editor2.model.text.OffsetPoint;
 import com.mammb.code.editor2.model.text.Textual;
-
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -53,6 +52,48 @@ public interface TextLine extends Textual {
      * @return the text runs
      */
     List<TextRun> runs();
+
+    /**
+     * Get the x position from the specified x offset.
+     * @param offset the specified char offset(total)
+     * @return the x position from the specified x offset
+     */
+    default double offsetToX(int offset) {
+        int start = point().offset();
+        int end   = start + length();
+        if (start > offset || offset >= end) {
+            throw new IndexOutOfBoundsException(
+                "start:%d end:%d offset:%d".formatted(start, end, offset));
+        }
+        for (TextRun run : runs()) {
+            if (run.length() == 0) continue;
+            int runStart = run.source().point().offset() + run.start();
+            int runEnd = runStart + run.length();
+            if (runStart <= offset && offset < runEnd) {
+                return run.offsetToX().apply(offset - run.offset());
+            }
+        }
+        throw new IllegalStateException("internal error:" + runs());
+    }
+
+    /**
+     * Get the char offset(total) from the specified x position.
+     * @param x the specified x position
+     * @return the char offset(total) from the specified x position
+     */
+    default int xToOffset(double x) {
+        int offset = point().offset();
+        for (TextRun run : runs()) {
+            double runStart = run.layout().x();
+            double runEnd   = runStart + run.layout().width();
+            if (runStart <= x && x < runEnd) {
+                offset += run.xToOffset().apply(x - run.layout().x());
+                return offset;
+            }
+            offset += run.length();
+        }
+        return offset;
+    }
 
     /**
      * Create a new TextLine.
