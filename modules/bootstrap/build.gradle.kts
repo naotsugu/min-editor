3588349fd5ef53d436f9f2607f2c324b0061f791
Naotsugu Kobayashi <naotsugukobayashi@gmail.com>
2023-08-29 17:31:03 +0900

    Add uber jar task

--- modules/bootstrap/build.gradle.kts
+++ modules/bootstrap/build.gradle.kts
@@ -11,3 +11,21 @@ application {
     mainClass.set("com.mammb.code.editor.bootstrap.Main")
     mainModule.set("code.editor.bootstrap")
 }
+
+tasks.register<Jar>("uberJar") {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    archiveClassifier.set("min-editor")
+
+    from(sourceSets.main.get().output)
+
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get()
+            .filter { it.name.endsWith("jar") }
+            .map { zipTree(it) }
+    })
+
+    manifest {
+        attributes("Main-Class" to "com.mammb.code.editor.bootstrap.Main")
+    }
+}
