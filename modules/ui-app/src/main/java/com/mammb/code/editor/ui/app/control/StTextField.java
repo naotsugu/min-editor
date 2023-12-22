da18d898e4e57d0e4bca4aa487c050f72d6236e1
Naotsugu Kobayashi <naotsugukobayashi@gmail.com>
2023-12-22 23:14:51 +0900

    Add style theme

---
+++ modules/ui-app/src/main/java/com/mammb/code/editor/ui/app/control/StTextField.java
@@ -0,0 +1,6 @@
+package com.mammb.code.editor.ui.app.control;
+
+import javafx.scene.control.TextField;
+
+public class StTextField extends TextField {
+}
--- modules/ui-app/src/main/java/com/mammb/code/editor/ui/app/control/StyleTheme.java
+++ modules/ui-app/src/main/java/com/mammb/code/editor/ui/app/control/StyleTheme.java
@@ -1,5 +1,32 @@
+/*
+ * Copyright 2019-2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.mammb.code.editor.ui.app.control;
 
+/**
+ * The StyleTheme.
+ * @author Naotsugu Kobayashi
+ */
 public class StyleTheme {
     String base = "#26252D";
+    String text = "#CACACE";
+    String back = "#1A1A1F";
+    String accent = "#3A587F";
+
+    String hover = "#42424A";
+    String focused = "";
+    String disabled = base;
+
 }
