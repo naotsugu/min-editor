edb6623e55e87d2c0dc8081b838532634bbf0e25
Naotsugu Kobayashi <naotsugukobayashi@gmail.com>
2024-02-05 18:18:31 +0900

    Add SelectionRange

---
+++ modules/ui-model/src/main/java/com/mammb/code/editor/ui/model/SelectionRange.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2023-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.mammb.code.editor.ui.model;
+
+import com.mammb.code.editor.model.text.OffsetPoint;
+import com.mammb.code.editor.model.text.OffsetPointRange;
+import java.util.List;
+
+/**
+ * SelectionRange supplier.
+ * @author Naotsugu Kobayashi
+ */
+public interface SelectionRange {
+
+    /**
+     * Get the OffsetPointRange list.
+     * @return the OffsetPointRange list
+     */
+    List<OffsetPointRange> getRanges();
+
+
+    /**
+     * Create the fix point range.
+     * @param start the start offset point
+     * @param end the end offset point
+     * @return the fix point range
+     */
+    static SelectionRange of(OffsetPoint start, OffsetPoint end) {
+        return () -> List.of(OffsetPointRange.of(start, end));
+    }
+
+
+    /**
+     * Create the open point range.
+     * @param point the start offset point
+     * @return the open point range
+     */
+    static OpenSelectionRange openOf(OffsetPoint point) {
+        return new OpenSelectionRange(point);
+    }
+
+
+    class OpenSelectionRange implements SelectionRange {
+
+        private final OffsetPoint start;
+        private List<OffsetPointRange> ranges = List.of();
+
+        OpenSelectionRange(OffsetPoint start) {
+            this.start = start;
+        }
+
+        public void to(OffsetPoint end) {
+            ranges = List.of(OffsetPointRange.of(start, end));
+        }
+
+        @Override
+        public List<OffsetPointRange> getRanges() {
+            return ranges;
+        }
+    }
+
+}
--- modules/ui-model/src/main/java/com/mammb/code/editor/ui/model/draw/Draws.java
+++ modules/ui-model/src/main/java/com/mammb/code/editor/ui/model/draw/Draws.java
@@ -17,9 +17,12 @@ package com.mammb.code.editor.ui.model.draw;
 
 import com.mammb.code.editor.javafx.layout.FxFonts;
 import com.mammb.code.editor.model.layout.TextRun;
+import com.mammb.code.editor.model.text.OffsetPoint;
+import com.mammb.code.editor.model.text.OffsetPointRange;
 import com.mammb.code.editor.ui.model.RectBox;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import java.util.Objects;
 
 /**
  * Draw utilities.
@@ -27,6 +30,14 @@ import javafx.scene.paint.Color;
  */
 public class Draws {
 
+    /**
+     * Draw the special character.
+     * @param gc the graphics context
+     * @param run the text run
+     * @param top
+     * @param lineHeight
+     * @param textLeft
+     */
     public static void specialCharacter(
             GraphicsContext gc,
             TextRun run,
@@ -62,7 +73,52 @@ public class Draws {
                 SpecialCharacter.WSP.draw(gc, rect);
             }
         }
-
     }
+
+
+    /**
+     * Draw the selection.
+     * @param gc the graphics context
+     * @param run the text run
+     * @param offsetY the position y
+     * @param left the left position of run(margin included)
+     * @param range the range of selection
+     * @param color the color
+     */
+    public static void selection(GraphicsContext gc, TextRun run, double offsetY, double left, OffsetPointRange range, Color color) {
+
+        final OffsetPoint min = range.minOffsetPoint();
+        final OffsetPoint max = range.maxOffsetPoint();
+
+        if (min == null || Objects.equals(min, max)) {
+            return;
+        }
+
+        long runStart = run.offset();
+        long runEnd = runStart + run.length();
+
+        if (max.offset() >= runStart && min.offset() < runEnd) {
+
+            final String text = run.text();
+
+            if (runEnd <= max.offset() &&
+                ((text.length() == 1 && text.charAt(0) == '\n') ||
+                    (text.length() == 2 && text.charAt(0) == '\r' && text.charAt(1) == '\n'))) {
+
+                gc.setFill(color);
+                gc.fillRect(run.layout().x() + left, offsetY, FxFonts.uppercaseLetterWidth(gc.getFont()), run.textLine().leadingHeight());
+
+            } else {
+
+                double x1 = run.offsetToX().apply(Math.toIntExact(Math.max(min.offset(), runStart) - runStart));
+                double x2 = run.offsetToX().apply(Math.toIntExact(Math.min(max.offset(), runEnd) - runStart));
+
+                gc.setFill(color);
+                gc.fillRect(x1 + left, offsetY, x2 - x1, run.textLine().leadingHeight());
+
+            }
+        }
+    }
+
 
 }
