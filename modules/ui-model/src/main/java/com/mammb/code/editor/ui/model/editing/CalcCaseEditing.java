fee511375746e09edfefd39c3cafa94c549c566e
Naotsugu Kobayashi <naotsugukobayashi@gmail.com>
2024-02-28 21:43:45 +0900

    Modify CalcCaseEditing

--- modules/ui-model/src/main/java/com/mammb/code/editor/ui/model/editing/CalcCaseEditing.java
+++ modules/ui-model/src/main/java/com/mammb/code/editor/ui/model/editing/CalcCaseEditing.java
@@ -18,9 +18,14 @@ package com.mammb.code.editor.ui.model.editing;
 import com.mammb.code.editor.ui.model.Editing;
 import com.mammb.code.editor.ui.model.EditorModel;
 import com.mammb.code.editor.ui.model.EditorQuery;
+import java.math.BigDecimal;
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+import java.util.stream.Collectors;
 
 /**
- * Hex case editing.
+ * Calc case editing.
  * @author Naotsugu Kobayashi
  */
 public class CalcCaseEditing implements Editing {
@@ -33,6 +38,113 @@ public class CalcCaseEditing implements Editing {
         }
 
         return false;
+    }
+
+
+    static String calc(String text) {
+        List<String> rpn = infixToRpn(text);
+        BigDecimal result = evaluate(rpn);
+        return result.toPlainString();
+    }
+
+    static List<String> infixToRpn(String text) {
+        List<String> tokens = Arrays.stream(text.splitWithDelimiters("\\W", -1))
+            .filter(Predicate.not(String::isBlank)).toList();
+        return shuntingYard(tokens);
+    }
+
+    static BigDecimal evaluate(List<String> rpn) {
+        if (rpn == null || rpn.isEmpty()) {
+            return BigDecimal.ZERO;
+        }
+        Stack<String> stack = new Stack<>();
+        for (String token : rpn) {
+            stack.push(switch (token) {
+                case "+" -> new BigDecimal(stack.pop()).add(new BigDecimal(stack.pop())).toPlainString();
+                case "-" -> new BigDecimal(stack.pop()).subtract(new BigDecimal(stack.pop())).toPlainString();
+                case "*" -> new BigDecimal(stack.pop()).multiply(new BigDecimal(stack.pop())).toPlainString();
+                case "/" -> new BigDecimal(stack.pop()).divide(new BigDecimal(stack.pop())).toPlainString();
+                case "^" -> new BigDecimal(stack.pop()).pow(Integer.parseInt(stack.pop())).toPlainString();
+                default -> token;
+            });
+        }
+        return new BigDecimal(stack.pop());
+    }
+
+
+    private enum Operator {
+        ADD("+", false, 0),
+        SUB("-", true,  0),
+        DIV("/", false, 5),
+        MUL("*", false, 5),
+        POW("^", true, 10);
+
+        final String symbol;
+        final boolean rightAssociative;
+        final int precedence;
+
+        Operator(String symbol, boolean rightAssociative, int precedence) {
+            this.symbol = symbol;
+            this.rightAssociative = rightAssociative;
+            this.precedence = precedence;
+        }
+
+        public boolean isRightAssociative() {
+            return rightAssociative;
+        }
+
+        public boolean isLeftAssociative() {
+            return !isRightAssociative();
+        }
+
+        public String symbol() {
+            return symbol;
+        }
+
+        public int comparePrecedence(Operator operator) {
+            return this.precedence - operator.precedence;
+        }
+
+        final static Map<String, Operator> values = Arrays.stream(Operator.values())
+            .collect(Collectors.toMap(Operator::symbol, UnaryOperator.identity()));
+    }
+
+
+    private static List<String> shuntingYard(List<String> tokens) {
+
+        List<String> output = new ArrayList<>();
+        Stack<String> stack = new Stack<>();
+
+        for (String token: tokens) {
+            if (Operator.values.containsKey(token)) {
+                while (!stack.isEmpty() && Operator.values.containsKey(stack.peek())) {
+                    Operator cOp = Operator.values.get(token);
+                    Operator lOp = Operator.values.get(stack.peek());
+                    if ((cOp.isLeftAssociative() && cOp.comparePrecedence(lOp) <= 0) ||
+                        (cOp.isRightAssociative() && cOp.comparePrecedence(lOp) < 0)) {
+                        output.add(stack.pop());
+                        continue;
+                    }
+                    break;
+                }
+                stack.push(token);
+            } else if ("(".equals(token)) {
+                stack.push(token);
+            } else if (")".equals(token)) {
+                while (!stack.isEmpty() && !stack.peek().equals("(")) {
+                    output.add(stack.pop());
+                }
+                stack.pop();
+            } else {
+                output.add(token);
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            output.add(stack.pop());
+        }
+
+        return output;
     }
 
 }
