9a3dc8471f430b9b0fe894dae8567125726b3613
Naotsugu Kobayashi <naotsugukobayashi@gmail.com>
2024-11-01 23:41:38 +0900

    Modify point

--- modules/core/src/main/java/com/mammb/code/editor/core/Point.java
+++ modules/core/src/main/java/com/mammb/code/editor/core/Point.java
@@ -21,18 +21,19 @@ package com.mammb.code.editor.core;
  */
 public interface Point extends Comparable<Point> {
 
-    Point zero = new PointRec(0, 0);
+    Point zero = of(0, 0);
 
     int row();
 
     int col();
 
     static Point of(int row, int col) {
+        record PointRec(int row, int col) implements Point { }
         return new PointRec(row, col);
     }
 
-    static <E extends Point> Point of(E point) {
-        return new PointRec(point);
+    static <E extends Point> Point of(E p) {
+        return of(p.row(), p.col());
     }
 
     default boolean isZero() {
@@ -49,10 +50,6 @@ public interface Point extends Comparable<Point> {
         }
     }
 
-    record PointRec(int row, int col) implements Point {
-        PointRec(Point p) { this(p.row(), p.col()); }
-    }
-
     record Range(Point start, Point end) implements Comparable<Range> {
         public Point min() {
             return start.compareTo(end) < 0 ? start : end;
@@ -60,7 +57,6 @@ public interface Point extends Comparable<Point> {
         public Point max() {
             return start.compareTo(end) > 0 ? start : end;
         }
-
         @Override
         public int compareTo(Range o) {
             return min().compareTo(o.min());
---
+++ modules/core/src/main/java/com/mammb/code/editor/core/editing/ToUpperCase.java
@@ -0,0 +1,10 @@
+package com.mammb.code.editor.core.editing;
+
+import com.mammb.code.editor.core.EditorModel;
+
+public class ToUpperCase implements Editing {
+    @Override
+    public boolean apply(EditorModel model) {
+        return false;
+    }
+}
