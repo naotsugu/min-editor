587f551243d40172989887f4086f081684af75b0
Naotsugu Kobayashi <naotsugukobayashi@gmail.com>
2024-11-15 20:29:36 +0900

    Add FloatBar

--- modules/ui-fx/src/main/java/com/mammb/code/editor/fx/FloatBar.java
+++ modules/ui-fx/src/main/java/com/mammb/code/editor/fx/FloatBar.java
@@ -1,23 +1,20 @@
 package com.mammb.code.editor.fx;
 
 import com.mammb.code.editor.core.Theme;
-import javafx.geometry.Bounds;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollBar;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
-import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 
 public class FloatBar extends HBox {
 
-    private final Text text = new Text();
+    private final Label label = new Label();
 
     public FloatBar(ScrollBar vScroll, ScrollBar hScroll) {
 
@@ -27,33 +24,33 @@ public class FloatBar extends HBox {
             new CornerRadii(2),
             Insets.EMPTY)));
 
-        text.setFont(Font.font("Consolas", 12));
-        text.setFill(Color.web(Theme.dark.fgColor()));
-        text.setText(" UTF-8 LF ");
-        Bounds textBounds = text.getLayoutBounds();
+        label.setText("UTF-8 LF");
+        label.setStyle("""
+            -fx-font: 12px "Consolas";
+            """);
 
-        vScroll.layoutBoundsProperty().addListener((ob, o , n) -> {
-            setLayoutY(n.getHeight() - textBounds.getHeight());
-        });
-        hScroll.layoutBoundsProperty().addListener((ob, o , n) -> {
-            setLayoutX(n.getWidth() - vScroll.getWidth() - textBounds.getWidth());
-        });
-        hScroll.visibleProperty().addListener((ob, o , visible) -> {
-            if (visible) {
-                setLayoutY(vScroll.getHeight() - textBounds.getHeight() - hScroll.getHeight());
-            } else {
-                setLayoutY(vScroll.getHeight() - textBounds.getHeight());
-            }
-        });
+        vScroll.layoutBoundsProperty().addListener((ob, o , n) -> layout(vScroll, hScroll));
+        hScroll.layoutBoundsProperty().addListener((ob, o , n) -> layout(vScroll, hScroll));
+        hScroll.visibleProperty().addListener((ob, o , n) -> layout(vScroll, hScroll));
+        layoutBoundsProperty().addListener((ob, o , n) -> layout(vScroll, hScroll));
 
-        getChildren().add(new StackPane(text));
+        getChildren().add(label);
+        layout(vScroll, hScroll);
         setAlignment(Pos.BASELINE_CENTER);
-        setPrefSize(textBounds.getWidth(), textBounds.getHeight());
+        setPrefSize(100, 15);
         setMinSize(Region.USE_PREF_SIZE, Region.USE_PREF_SIZE);
         setMaxSize(Region.USE_PREF_SIZE, Region.USE_PREF_SIZE);
+        setWidth(100);
+        setHeight(15);
+    }
 
-        setWidth(textBounds.getWidth());
-        setHeight(textBounds.getHeight());
+    private void layout(ScrollBar vScroll, ScrollBar hScroll) {
+        setLayoutX(hScroll.getWidth() - vScroll.getWidth() - getWidth());
+        if (hScroll.isVisible()) {
+            setLayoutY(vScroll.getHeight() - getHeight() - hScroll.getHeight());
+        } else {
+            setLayoutY(vScroll.getHeight() - getHeight());
+        }
     }
 
 }
