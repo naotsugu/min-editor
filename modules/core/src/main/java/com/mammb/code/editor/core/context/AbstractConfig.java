da89b26d7b6257a18ababcdf812a34e725801e43
Naotsugu Kobayashi <naotsugukobayashi@gmail.com>
2025-02-24 12:12:57 +0900

    Modify context

--- modules/core/src/main/java/com/mammb/code/editor/core/context/AbstractConfig.java
+++ modules/core/src/main/java/com/mammb/code/editor/core/context/AbstractConfig.java
@@ -15,7 +15,11 @@
  */
 package com.mammb.code.editor.core.context;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -24,6 +28,9 @@ import java.util.concurrent.ConcurrentHashMap;
  * @author Naotsugu Kobayashi
  */
 public abstract class AbstractConfig implements Config {
+
+    /** The logger. */
+    private static final System.Logger log = System.getLogger(AbstractConfig.class.getName());
 
     /** The props map. */
     private final Map<String, Object> props = new ConcurrentHashMap<>();
@@ -56,6 +63,69 @@ public abstract class AbstractConfig implements Config {
             default -> "15";
         };
         return Double.parseDouble(props.getOrDefault("fontSize", defaultValue).toString());
+    }
+
+    @Override
+    public void load() {
+        try {
+            for (String line : Files.readAllLines(propsPath)) {
+                var str = line.trim();
+                if (str.startsWith("#") || str.startsWith("!") || !str.contains("=")) continue;
+                var kv = str.split("=", 2);
+                props.put(kv[0], kv[1]);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void save() {
+        try {
+            List<String> merged = new ArrayList<>();
+            for (String line : Files.readAllLines(propsPath)) {
+                var str = line.trim();
+                if (str.startsWith("#") || str.startsWith("!") || !str.contains("=")) {
+                    merged.add(line);
+                    continue;
+                }
+                var kv = str.split("=", 2);
+                if (props.containsKey(kv[0])) {
+                    var v = props.remove(kv[0]);
+                    merged.add(kv[0] + "=" + v);
+                }
+            }
+            for (var entry : props.entrySet()) {
+                merged.add(entry.getKey() + "=" + entry.getValue());
+            }
+            Files.write(propsPath, merged);
+        } catch (IOException ignore) {
+            log.log(System.Logger.Level.ERROR, ignore);
+        }
+    }
+
+    public void clear() {
+        try {
+            Files.delete(propsPath);
+            log.log(System.Logger.Level.INFO, "deleted config file.");
+        } catch (IOException ignore) {
+            log.log(System.Logger.Level.ERROR, ignore);
+        }
+    }
+
+
+    /**
+     * Get the configuration directory for platform.
+     * @return the configuration directory
+     */
+    protected Path configRoot() {
+        Path home = Path.of(System.getProperty("user.home"));
+        return switch (Context.platform) {
+            case "windows" -> home.resolve("AppData", "Local");
+            case "mac" -> home.resolve("Library", "Application Support");
+            case "linux" -> home.resolve(".config");
+            default -> home;
+        };
     }
 
 }
--- modules/core/src/main/java/com/mammb/code/editor/core/context/Config.java
+++ modules/core/src/main/java/com/mammb/code/editor/core/context/Config.java
@@ -33,5 +33,8 @@ public interface Config {
      */
     double fontSize();
 
+    void load();
+
+    void save();
 
 }
