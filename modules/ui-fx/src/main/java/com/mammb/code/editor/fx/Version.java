e9851507e1068e0a22702b13d4540395427161ee
Naotsugu Kobayashi <naotsugukobayashi@gmail.com>
2025-07-16 18:33:13 +0900

    Release v0.2.3

--- modules/ui-fx/src/main/java/com/mammb/code/editor/fx/Version.java
+++ modules/ui-fx/src/main/java/com/mammb/code/editor/fx/Version.java
@@ -25,7 +25,7 @@ public interface Version {
     String appName = "min-editor";
 
     /** The version value. */
-    String val = "0.2.2";
+    String val = "0.2.3";
 
     /**
      * Extracts and returns the major version number from the version string.
