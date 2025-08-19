41c9c236a23f915cd47cd0684709bbb0c66e6af0
Naotsugu Kobayashi <naotsugukobayashi@gmail.com>
2025-08-19 17:52:42 +0900

    docs: added java doc

--- modules/ui-fx/src/main/java/com/mammb/code/editor/fx/ContentPane.java
+++ modules/ui-fx/src/main/java/com/mammb/code/editor/fx/ContentPane.java
@@ -28,18 +28,46 @@ import java.util.function.Consumer;
  */
 public abstract class ContentPane extends StackPane {
 
+    /**
+     * Focus the pane.
+     */
     abstract void focus();
 
+    /**
+     * Check whether the pane needs close confirmation.
+     * @return {@code true}, if the pane needs close confirmation
+     */
     abstract boolean needsCloseConfirmation();
 
+    /**
+     * Check whether the pane can be closed.
+     * @return {@code true}, if the pane can be closed
+     */
     abstract boolean canClose();
 
+    /**
+     * Close the pane.
+     * @param force {@code true}, if the pane should be closed even if it needs close confirmation
+     * @return the session if closed, otherwise {@code Optional.empty()}
+     */
     abstract Optional<Session> close(boolean force);
 
+    /**
+     * Set the close listener.
+     * @param closeListener the close listener
+     */
     abstract void setCloseListener(Consumer<ContentPane> closeListener);
 
+    /**
+     * Get the name property.
+     * @return the name property
+     */
     abstract ReadOnlyObjectProperty<Name> nameProperty();
 
+    /**
+     * Check whether the pane is external changed.
+     * @return {@code true}, if the pane is external changed
+     */
     abstract boolean externalChanged();
 
 }
