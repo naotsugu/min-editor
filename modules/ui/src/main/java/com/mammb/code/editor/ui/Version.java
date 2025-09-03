28f2da4ba09428ab6531d4f3285b1c57197fcf5b
Naotsugu Kobayashi <naotsugukobayashi@gmail.com>
2025-09-03 17:35:22 +0900

    Release v0.2.4

--- modules/ui/src/main/java/com/mammb/code/editor/ui/Version.java
+++ modules/ui/src/main/java/com/mammb/code/editor/ui/Version.java
@@ -25,7 +25,7 @@ public interface Version {
     String appName = "min-editor";
 
     /** The version value. */
-    String val = "0.2.3";
+    String val = "0.2.4";
 
     /**
      * Extracts and returns the major version number from the version string.
