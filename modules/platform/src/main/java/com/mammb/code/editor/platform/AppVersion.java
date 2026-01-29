0a82f8468090cf7410a9881d160e265812e91bde
Naotsugu Kobayashi <naotsugukobayashi@gmail.com>
2026-01-29 18:17:42 +0900

    Release v0.5.3

--- modules/platform/src/main/java/com/mammb/code/editor/platform/AppVersion.java
+++ modules/platform/src/main/java/com/mammb/code/editor/platform/AppVersion.java
@@ -25,7 +25,7 @@ public interface AppVersion {
     String appName = "min-editor";
 
     /** The version value. */
-    Version val = Version.of("0.5.2");
+    Version val = Version.of("0.5.3");
 
     /**
      * An immutable representation of a version number consisting of three parts:
