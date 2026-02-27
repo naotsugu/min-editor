53a1b21795e31060abbcaf8e8f6cb41c06b36aaf
Naotsugu Kobayashi <naotsugukobayashi@gmail.com>
2026-02-28 07:05:05 +0900

    feat: added PaintPulse

---
+++ modules/ui-fx/src/main/java/com/mammb/code/editor/ui/fx/PaintPulse.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2023-2026 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.mammb.code.editor.ui.fx;
+
+import javafx.animation.AnimationTimer;
+
+/**
+ * PaintTimer.
+ * @author Naotsugu Kobayashi
+ */
+public class PaintPulse extends AnimationTimer {
+
+    private volatile boolean dirty = false;
+
+    private final Runnable paint;
+
+    /**
+     * Constructor.
+     * @param paint the paint runnable
+     */
+    public PaintPulse(Runnable paint) {
+        this.paint = paint;
+    }
+
+    /**
+     * Marks the animation as dirty, indicating that the paint action should
+     * be executed during the next handle call in the animation cycle.
+     */
+    public void request() {
+        dirty = true;
+    }
+
+    @Override
+    public void handle(long l) {
+        if (dirty) {
+            paint.run();
+            dirty = false;
+        }
+    }
+
+}
