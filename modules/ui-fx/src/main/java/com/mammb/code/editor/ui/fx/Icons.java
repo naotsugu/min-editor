eea195fd681d2bf841ef055f8958582f826afd44
Naotsugu Kobayashi <naotsugukobayashi@gmail.com>
2026-03-10 20:10:36 +0900

    feat: added icon

--- modules/ui-fx/src/main/java/com/mammb/code/editor/ui/fx/Icons.java
+++ modules/ui-fx/src/main/java/com/mammb/code/editor/ui/fx/Icons.java
@@ -35,6 +35,17 @@ public class Icons {
             """);
     }
 
+    /**
+     * Get the exclamation circle icon.
+     * @return the exclamation circle icon
+     */
+    public static SVGPath exclamationCircle() {
+        return svg("""
+            M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16
+            M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z
+            """);
+    }
+
     private static SVGPath svg(String d) {
         var svg = new SVGPath();
         svg.setContent(d);
