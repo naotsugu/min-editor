e2a486b5b5ad66791cd7723c2df4645858ca4a14
Naotsugu Kobayashi <naotsugukobayashi@gmail.com>
2026-03-11 20:36:32 +0900

    feat: added close icon

--- modules/ui-fx/src/main/java/com/mammb/code/editor/ui/fx/Icons.java
+++ modules/ui-fx/src/main/java/com/mammb/code/editor/ui/fx/Icons.java
@@ -46,6 +46,16 @@ public class Icons {
             """);
     }
 
+    /**
+     * Get the close icon.
+     * @return the close icon as an SVGPath
+     */
+    public static SVGPath close() {
+        return svg("""
+            M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708
+            """);
+    }
+
     private static SVGPath svg(String d) {
         var svg = new SVGPath();
         svg.setContent(d);
