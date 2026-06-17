cc1425d0d8b091e647270e0ec49673d24fa8d21b
Naotsugu Kobayashi <naotsugukobayashi@gmail.com>
2026-06-17 18:31:59 +0900

    docs: modified java doc

--- modules/core/src/main/java/com/mammb/code/editor/core/syntax/lang/TsvSyntax.java
+++ modules/core/src/main/java/com/mammb/code/editor/core/syntax/lang/TsvSyntax.java
@@ -24,7 +24,7 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- * The tsv editor model.
+ * The tsv syntax.
  * @author Naotsugu Kobayashi
  */
 public class TsvSyntax implements Syntax {
